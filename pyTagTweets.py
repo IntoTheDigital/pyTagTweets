d1e0d58b506c62b8659ee5ea6dd1cf29c967449c
John Byron <giorgio.cavaggion@gmail.com>
2013-08-09 17:46:46 -0500

    No more dependency on string package

--- pyTagTweets.py
+++ pyTagTweets.py
@@ -1,14 +1,18 @@
-# import pygame
 from pytagcloud import create_tag_image, make_tags, LAYOUTS
 from pytagcloud.lang.counter import get_tag_counts
 from requests_oauthlib import OAuth1
 import requests
 import operator
-import string
 from collections import Counter
+import time
 
 query = 'chicago'
-limit = 100
+limit = 200
+layout = 3
+background_color = (255, 255, 255)
+max_word_size = 80
+max_words = 180
+wait_time = 10  # time interval between two tag clouds are created in seconds
 
 f = open('configuration.txt', 'rb')
 my_secrets = [line.strip() for line in f]
@@ -20,10 +24,10 @@ my_oauth = OAuth1(my_secrets[0],
 complete_url = 'https://api.twitter.com/1.1/search/tweets.json?q=' + query + '&count=' + str(limit)
 
 f = open('stopwords.txt', 'rb')
-tweets_words = [query, '#' + query, 'http', 'amp']
-stop_words = [line.strip() for line in f]
-stop_words += tweets_words
-exclude = set(string.punctuation.replace('#', '').replace('@', ''))
+stop_words = [line.strip() for line in f]  # loading common English stopwords from file
+tweets_words = [query, '#' + query, 'http', 'amp']  # common words in tweets
+stop_words += tweets_words  # add common words in tweets to stopwords
+punctuation = "!\"$%&'()*+,-./:;<=>?[\]^_`{|}~'"
 
 while True:
     my_text = ''
@@ -31,7 +35,7 @@ while True:
     tweets = r.json()
     for tweet in tweets['statuses']:
         text = tweet['text'].lower()
-        text = ''.join(ch for ch in text if ch not in exclude)
+        text = ''.join(ch for ch in text if ch not in punctuation)  # exclude punctuation from tweets
         important_words = text
         my_text += important_words
 
@@ -44,9 +48,10 @@ while True:
         if len(key) < 3 or key.startswith('http'):
             del counts[key]
 
-    final = counts.most_common(180)
+    final = counts.most_common(max_words)
     max_count = max(final, key=operator.itemgetter(1))[1]
     final = [(name, count / float(max_count))for name, count in final]
-    tags = make_tags(final, maxsize=80)
-    create_tag_image(tags, 'cloud_large.png', size=(1280, 800), layout=3, fontname='Lobster', background = (255, 255, 255))
+    tags = make_tags(final, maxsize=max_word_size)
+    create_tag_image(tags, 'cloud_large.png', size=(1280, 800), layout=layout, fontname='Lobster', background = background_color)
     print "new png created"
+    time.sleep(wait_time)
